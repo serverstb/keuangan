63c6e7b05a478af2e758edfc1ebc7c9232ea4c0f
Ryan Dewantoro <pusdakom@gmail.com>
2025-11-22 00:49:34 +0000

    Try fixing this error: `Console Error: [Turbopack HMR] Expected module t

---
+++ src/lib/placeholder-images.js
@@ -0,0 +1,4 @@
+const data = require('./placeholder-images.json');
+/** @type {import('./placeholder-images').ImagePlaceholder[]} */
+const PlaceHolderImages = data.placeholderImages;
+module.exports = { PlaceHolderImages };
--- src/lib/placeholder-images.ts
+++ src/lib/placeholder-images.ts
@@ -1,10 +1,5 @@
+import type { ImagePlaceholder } from './placeholder-images.js';
 import data from './placeholder-images.json';
 
-export type ImagePlaceholder = {
-  id: string;
-  description: string;
-  imageUrl: string;
-  imageHint: string;
-};
-
-export const PlaceHolderImages: ImagePlaceholder[] = data.placeholderImages;
+export { PlaceHolderImages } from './placeholder-images.js';
+export type { ImagePlaceholder };
